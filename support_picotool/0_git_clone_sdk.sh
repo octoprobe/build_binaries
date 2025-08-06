88c0e25df62a224228329a564cc783e9394cd317
Hans Maerki <buhtig.hans.maerki@ergoinfo.ch>
2025-08-06 13:02:08 +0200

    picotool/sdk v2.1.1 -> v2.1.0 -> 2.2.0

--- support_picotool/0_git_clone_sdk.sh
+++ support_picotool/0_git_clone_sdk.sh
@@ -6,6 +6,6 @@ cd $WORKDIR
 
 rm -rf pico-sdk
 
-git clone --depth 1 --branch 2.1.1 https://github.com/raspberrypi/pico-sdk.git
+git clone --depth 1 --branch 2.2.0 https://github.com/raspberrypi/pico-sdk.git
 
 (cd pico-sdk && git submodule update --depth 1 --init lib/mbedtls)
--- support_picotool/1_git_clone_picotool.sh
+++ support_picotool/1_git_clone_picotool.sh
@@ -6,6 +6,6 @@ cd $WORKDIR
 
 rm -rf picotool
 
-git clone --depth 1 --branch 2.1.1 https://github.com/raspberrypi/picotool.git
+git clone --depth 1 --branch 2.2.0 https://github.com/raspberrypi/picotool.git
 
 cd picotool
